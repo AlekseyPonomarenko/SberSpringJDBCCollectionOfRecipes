a7c04b3db6a863ce4488f436c9e78621d51d325b
Aleksey Ponomarenko <firebird89@list.ru>
2021-02-21 18:24:29 +0300

    Ingridient

--- src/main/java/model/Ingridient.java
+++ src/main/java/model/Ingridient.java
@@ -1,4 +1,54 @@
 package model;
 
 public class Ingridient {
+
+    private Integer id;
+    private String item;
+    private double count;
+    private String unit;
+
+    public static Ingridient create(String item, double count, String unit) {
+
+        Ingridient ingridient = new Ingridient();
+        ingridient.setItem(item);
+        ingridient.setCount(count);
+        ingridient.setUnit(unit);
+        return ingridient;
+
+    }
+
+    private Ingridient() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id=id;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item=item;
+    }
+
+    public double getCount() {
+        return count;
+    }
+
+    public void setCount(double count) {
+        this.count=count;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit=unit;
+    }
 }
